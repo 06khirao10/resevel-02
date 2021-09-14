efe43735d97885bc2b1b252ba570af2f28138760
akrsmz <inaligumiku@gmail.com>
2021-09-14 23:42:08 +0900

    リレーション修正とモデル修正

--- app/Reservation.php
+++ app/Reservation.php
@@ -6,6 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Reservation extends Model
 {
+    protected $fillable = ['requirements','start_datetime','end_datetime'];
+
     public function user()
     {
         return $this->belongsTo('App\User');
---
+++ app/ReservationNotPossible.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ReservationNotPossible extends Model
+{
+    protected $fillable = ['date'];
+}
