10c09bf2ea368f23b52515907277f28c65a31800
akrsmz <inaligumiku@gmail.com>
2021-09-14 21:26:41 +0900

    relation

--- app/User.php
+++ app/User.php
@@ -36,6 +36,10 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-}
 
-test
+    public function reservation()
+    {
+        return $this->hasOne('App\Reservation');
+    }
+
+}
