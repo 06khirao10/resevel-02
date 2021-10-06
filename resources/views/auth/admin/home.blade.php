0efd224a034e0a6bc79c034b925444a1dda8855e
高橋莉子 <mondescmpetit6@gmail.com>
2021-10-06 18:22:14 +0900

    微調整

--- resources/views/auth/admin/home.blade.php
+++ resources/views/auth/admin/home.blade.php
@@ -1,13 +1,11 @@
 @extends('layouts.app')
 
 @section('content')
-
-<div class="container">
-    <h3>管理者ホーム</h3>
-    @csrf
-    <div class="button">
-        <a href ="{{ route('admin.passwordEdit') }}">プロフィール</a>
+    <div class="container">
+        <h3>管理者ホーム</h3>
+            @csrf
+        <div class="button">
+            <a href ="{{ route('admin.passwordEdit') }}">プロフィール</a>
+        </div>
     </div>
-</div>
-
 @endsection
--- resources/views/auth/admin/passwordedit.blade.php
+++ resources/views/auth/admin/passwordedit.blade.php
@@ -1,5 +1,3 @@
-Passwordeditblade
-
 @extends('layouts.app')
 
 @section('content')
@@ -10,7 +8,7 @@ Passwordeditblade
                 <div class="row">
                     <div class="col-md-8 col-md-offset-2">
                         <div class="panel panel-default">
-                            <div class="panel-heading">Change Password -管理者-</div>
+                            <div class="panel-heading">管理者パスワード変更</div>
                                 <div class="panel-body">        
                                     @if (session('error'))
                                         <div class="alert alert-danger">
@@ -23,7 +21,7 @@ Passwordeditblade
                                         </div>
                                     @endif
                                         <div class="form-group{{ $errors->has('current-password') ? ' has-error' : '' }}">
-                                            <label for="new-password" class="col-md-4 control-label">Current Password</label>
+                                            <label for="new-password" class="col-md-4 control-label">現在のパスワード</label>
                                                 <div class="col-md-6">
                                                     <input id="current-password" type="password" class="form-control" name="current-password" required>
                                                         @if ($errors->has('current-password'))
@@ -34,7 +32,7 @@ Passwordeditblade
                                                 </div>
                                         </div>
                                         <div class="form-group{{ $errors->has('new-password') ? ' has-error' : '' }}">
-                                            <label for="new-password" class="col-md-4 control-label">New Password</label>
+                                            <label for="new-password" class="col-md-4 control-label">新しいパスワード</label>
                                                 <div class="col-md-6">
                                                     <input id="new-password" type="password" class="form-control" name="new-password" required>
                                                         @if ($errors->has('new-password'))
@@ -45,15 +43,13 @@ Passwordeditblade
                                                 </div>
                                         </div>
                                         <div class="form-group">
-                                            <label for="new-password-confirm" class="col-md-4 control-label">Confirm New Password</label>
+                                            <label for="new-password-confirm" class="col-md-4 control-label">確認用パスワード</label>
                                                 <div class="col-md-6">
                                                     <input id="new-password-confirm" type="password" class="form-control" name="new-password_confirmation" required>
                                                 </div>
                                         </div>
                                         <div class="form-group">
-                                            <button type="submit" class="btn btn-primary">
-                                                Change Password
-                                            </button>
+                                            <button type="submit" class="btn btn-primary">変更</button>
                                         </div>
                         </div>
                     </div>
