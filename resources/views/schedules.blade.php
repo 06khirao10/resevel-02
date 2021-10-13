06a2c2e5482abca3fe0dbcd930cb26abd17b784c
akrsmz <inaligumiku@gmail.com>
2021-10-13 21:13:33 +0900

    NG日設定画面微修正

--- resources/views/schedules.blade.php
+++ resources/views/schedules.blade.php
@@ -5,6 +5,8 @@
 <div>
     @foreach($seat_date as $list)
     <p>{{ $list }}</p>
+    <a href="{{ route('admin.schedules.store') }}">予約NG日にする</a>
+    <a href="{{ route('admin.schedules.store') }}">予約可能日にする</a>
     @endforeach
 </div>
 
