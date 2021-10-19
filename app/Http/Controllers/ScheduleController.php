dce57585d4d3124019ea1e92d970d833b3b529e2
akrsmz <inaligumiku@gmail.com>
2021-10-19 22:53:53 +0900

    予約NG日登録機能

--- app/Http/Controllers/ScheduleController.php
+++ app/Http/Controllers/ScheduleController.php
@@ -4,26 +4,49 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\ReservationNotPossible;
 use Carbon\Carbon;
 
 class ScheduleController extends Controller
 {
+    //NG日設定画面表示
     public function edit(){
         $admin = Auth::user();
 
         $firstOfMonth = Carbon::now()->firstOfMonth();
         $endOfMonth = Carbon::now()->endOfMonth();
-        $seat_date = [];
 
         for ($i = 0; true; $i++) {
             $date = $firstOfMonth->copy()->addDays($i);
             if ($date > $endOfMonth) {
             break;
             }
-            $day = date('Y/m/d',strtotime($date));
-            $dates[]=$day;
+            //今月の日付を配列に入れる
+            $dates[]=$date;
         }
 
-        return view('schedules',compact('admin','dates'));
+        $button = ReservationNotPossible::all();
+
+        return view('schedules',compact('admin','dates','button'));
+    }
+
+    //予約NG日登録
+    public function store(Request $request){
+        $date = $request->input('add');
+
+        ReservationNotPossible::create([
+            'date' => $date,
+        ]);
+
+        return back();
+    }
+
+    //予約可能日登録
+    public function destroy(Request $request){
+        $date = $request->input('remove');
+
+       ReservationNotPossible::where('date','=',$date) ->delete();
+
+        return back();
     }
 }
--- resources/views/schedules.blade.php
+++ resources/views/schedules.blade.php
@@ -4,9 +4,26 @@
 
 <div>
     @foreach($dates as $date)
-    <p>{{ $date }}</p>
-    <a href="{{ route('admin.schedules.store') }}">予約NG日にする</a>
-    <a href="{{ route('admin.schedules.store') }}">予約可能日にする</a>
+    <?php
+        //date型を文字列に
+        $date_str = date('Y/m/d',strtotime($date));
+    ?>
+    <p>{{ $date_str }}</p>
+
+    @if(!in_array($date,(array)$button))
+    <form action="{{ route('admin.schedules.store') }}" method="POST">
+        @csrf
+        <input type="hidden" name="add" value="{{ $date }}">
+        <button type="submit" name="">予約NG日にする</button>
+    </form>
+    @else
+    <form action="{{ route('admin.schedules.destroy') }}" method="POST">
+        @csrf
+        <input type="hidden" name="remove" value="{{ $date }}">
+        <button type="submit" name="">予約可能日にする</button>
+    </form>
+    @endif
+
     @endforeach
 </div>
 
